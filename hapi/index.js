bfb4d2bc2600c44d989ac8848c1687835e632d5b
Peter Griess <pg@std.in>
2013-07-09 08:53:51 -0500

    Munge Solr results into CrimeDB JSON API.

--- hapi/index.js
+++ hapi/index.js
@@ -3,60 +3,78 @@
  */
 
 var console = require('console');
-var crimedb_common = require('crimedb/common');
 var hapi = require('hapi');
+var request = require('request');
+var strptime = require('micro-strptime').strptime;
 
-crimedb_common.getDB(
-    'http://localhost:5984/crimes', false, false,
-    function(err, dbHandle) {
-        if (err) {
-            throw err;
-        }
+var api_handler = function(req) {
+    var FIELD_TRANSFORMATIONS = {
+        date: ['time', function(v) {
+            return strptime(v, '%Y-%m-%dT%H:%M:%S%Z').getTime() / 1000;
+        }],
+        description: ['description', function(v) { return v; }],
+    };
 
-        var api_handler = function(req) {
-            /* Default query parameters */
-            var couchQuery = {
-                limit: 100
-            };
+    /*
+     * Apply query parameters from the request
+     *
+     * XXX: Parameter validation, including types and ranges. Hapi can
+     *      do this sort of thing, AFAICT.
+     */
+    var solrQuery = {};
+    solrQuery.q = '*:*';
+    solrQuery.limit = Math.min(req.query.limit || 100, 1000);
+
+    request({
+        url: 'http://localhost:8080/solr-4.3.0/crime/query',
+        qs: solrQuery,
+        json: [],
+        headers: {'Accept': 'application/json'}},
+        function(err, resp, body) {
+            if (err) {
+                // XXX: Generate an absolute URI for the current
+                //      hostname. Write handler for each of these pages
+                //      so that they're actually useful.
+                req.reply(
+                    JSON.stringify({
+                        title: 'Internal error',
+                        problemType: 'http://localhost:8888/errors/Internal_error'}))
+                    .code(500)
+                    .type('application/api-problem+json');
+                return;
+            }
 
             /*
-             * Apply query parameters from the request
-             *
-             * XXX: Parameter validation, including types and ranges. Hapi can
-             *      do this sort of thing, AFAICT.
+             * TODO: Handle Solr errors here. There may be some in the response
+             *       format.
+             * TODO: Cursors.
              */
-            couchQuery.limit = req.query.limit || 100;
-
-            dbHandle.request({
-                    path: '/_design/foo/_view/bar',
-                    query: couchQuery,
-                }, function(err, result) {
-                    if (err) {
-                        // XXX: Generate an absolute URI for the current
-                        //      hostname. Write handler for each of these pages
-                        //      so that they're actually useful.
-                        req.reply(
-                            JSON.stringify({
-                                title: 'Internal error',
-                                problemType: 'http://localhost:8888/errors/Internal_error'}))
-                            .code(500)
-                            .type('application/api-problem+json');
-                        return;
+            var outDocs = body.response.docs.map(function(inDoc) {
+                var outDoc = {};
+                for (k in inDoc) {
+                    if (!(k in FIELD_TRANSFORMATIONS)) {
+                        continue;
                     }
 
-                    req.reply(JSON.stringify({results: result.rows}))
-                        .code(200)
-                        .type('application/vnd.crimedb.org+json')
+                    ft = FIELD_TRANSFORMATIONS[k];
+                    outDoc[ft[0]] = ft[1](inDoc[k]);
                 }
-            );
-        };
 
-        var server = hapi.createServer('localhost', 8888, {cors: true})
-        server.route([{
-                path: '/crimes',
-                method: 'GET',
-                handler: api_handler,
-        }]);
-        server.start();
-    }
-);
+                return outDoc;
+            });
+
+
+            req.reply(JSON.stringify({results: outDocs}))
+                .code(200)
+                .type('application/vnd.crimedb.org+json')
+        }
+    );
+};
+
+var server = hapi.createServer('localhost', 8888, {cors: true})
+server.route([{
+        path: '/crimes',
+        method: 'GET',
+        handler: api_handler,
+}]);
+server.start();
