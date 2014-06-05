a551fa13515b8e610662e19f983fa2e8e7940e07
Peter Griess <pg@std.in>
2014-06-05 15:18:35 -0500

    Add OSM relation viewer at /_/osm_view
    - Add /_/osm_view, which supports GET and POST. The former renders a
      form for populating the latter.

--- hapi/index.js
+++ hapi/index.js
@@ -2,9 +2,13 @@
  * Web application for CrimeDB.
  */
 
+var assert = require('assert');
 var console = require('console');
 var crimedb_common = require('crimedb/common.js');
+var fs = require('fs');
 var hapi = require('hapi');
+var mustache = require('mustache');
+var osm = require('crimedb/osm');
 var request = require('request');
 var strptime = require('micro-strptime').strptime;
 
@@ -48,7 +52,6 @@ var transformSolrDocument = (function() {
 })();
 
 var api_handler = function(req, reply) {
-
     /* Fail the request with our JSON formatting */
     var failRequest = function(title, problemType) {
         reply(
@@ -127,6 +130,10 @@ var api_handler = function(req, reply) {
         solrQuery.q = '*:*';
     }
 
+    /* TODO: Use XML response format so that we can stream the
+     *       response back rather than reading the entire thing?
+     *       Requires manually generating JSON.
+     */
     request({
         url: 'http://localhost:8080/solr-4.3.0/crime/query',
         qs: solrQuery,
@@ -147,6 +154,89 @@ var api_handler = function(req, reply) {
                 .type('application/vnd.crimedb.org+json')
         }
     );
+};
+
+var osm_view_handler = function(req, reply) {
+    assert('relation_id' in req.payload);
+    var relationID = req.payload.relation_id;
+
+    /* Pull out the list of nodes, ways and relations */
+    op = new osm.Parser();
+
+    var nodes = [];
+    op.on('node', function(n) {
+        nodes.push(n);
+    });
+
+    var ways = [];
+    op.on('way', function(w) {
+        ways.push(w);
+    });
+
+    var relations = [];
+    op.on('relation', function(r) {
+        relations.push(r);
+    });
+
+    op.on('end', function() {
+        /* Turn the relation into an ordered list of nodes */
+        var mapFromArray = function(a) {
+            var m = {};
+            a.forEach(function(o) {
+                m[o.attributes.id] = o;
+            });
+
+            return m;
+        };
+
+        var nodeMap = mapFromArray(nodes);
+        var wayMap = mapFromArray(ways);
+        var relationMap = mapFromArray(relations);
+
+        assert(relationID in relationMap);
+
+        /* Turn a node into a lat/lon object */
+        var pointFromNode = function(n) {
+            return {
+                lon: n.attributes.lon,
+                lat: n.attributes.lat,
+            };
+        };
+
+        /* Compute an ordered list of points for the relation */
+        var relationNIDs = osm.waysToContiguousNIDs(
+            relationMap[relationID].ways.map(function(wid) { return wayMap[wid]; }),
+            nodes);
+        var relationPoints = [];
+        relationNIDs.forEach(function(nid) {
+            relationPoints.push(pointFromNode(nodeMap[nid]));
+        });
+
+        /* Render our template */
+        var templateData = '';
+
+        var s = fs.createReadStream('static/osm_view.html');
+        s.on('data', function(s) {
+            templateData += s;
+        });
+
+        s.on('end', function() {
+            reply(
+                mustache.render(
+                    templateData, {
+                        mapNodes: relationPoints }))
+                .code(200)
+                .type('text/html');
+        });
+    });
+
+    op.on('error', function() {
+        reply('Failed to parse OSM file');
+        return;
+    });
+
+    assert('file' in req.payload);
+    op.parseXMLStream(req.payload.file);
 };
 
 var server = hapi.createServer('0.0.0.0', 8888, {
@@ -174,6 +264,20 @@ server.route([
         path: '/',
         method: 'GET',
         handler: { file: 'static/index.html' },
+    }, {
+        path: '/_/osm_view',
+        method: 'GET',
+        handler: { file: 'static/osm_view_form.html' },
+    }, {
+        path: '/_/osm_view',
+        method: 'POST',
+        handler: osm_view_handler,
+        config: {
+            payload: {
+                output: 'stream',
+                parse: true,
+            },
+        },
     }
 ]);
 server.pack.require(
