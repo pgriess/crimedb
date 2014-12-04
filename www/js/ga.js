482cc15fad47300488bc3565ba012e8c978dadff
Peter Griess <pg@std.in>
2014-12-04 13:04:45 -0600

    Add 'noga' cookie
    - If the ga.js module sees a 'noga' cookie set, return undefined from
      the loader. The idea here is to avoid counting CrimeDB developers in
      the Google Analytics stats
    - Set the noga cookie in httpd.conf for www.crimedb.org

--- www/js/ga.js
+++ www/js/ga.js
@@ -19,6 +19,15 @@
 
 define(
     function() {
+        // Disable Google Analytics if we have the 'noga' cookie set to 1
+        //
+        // This cookie gets set in the Apache configs for CrimeDB development.
+        // Although it's not set in production the cookie's domain and path
+        // will cause it to be sent to production as well.
+        if (document.cookie.search(/\bnoga=1\b/) >= 0) {
+            return undefined;
+        }
+
         (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
         (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
         m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
