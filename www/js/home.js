92b97dac226f628d12bb3230bf962fd3054d0dc8
Peter Griess <pg@std.in>
2015-03-20 10:38:51 -0500

    Add marker when looking up location directly.

--- www/js/home.js
+++ www/js/home.js
@@ -27,17 +27,40 @@ requirejs.config({
 requirejs(
     ['leaflet', 'jquery', 'gmaps', 'ga', 'crimedb-leaflet', 'stamen-leaflet'],
     function(L, $, gmaps, ga) {
+        /*
+         * This is necessary because by default LeafletJS expects icons to be
+         * available relative to the JS source path. This makes sense if we're
+         * using it via their CDN (which hosts the images), but not so much
+         * since we're hosting it ourseles (so that we can use AMD).
+         *
+         * TODO: We should either update the local copy of leaflet.js with the
+         *       below snippet, or figure out a better way of wrapping this in
+         *       an AMD definition such that that doesn't break.
+         */
+        L.Icon.Default.imagePath = '//cdn.leafletjs.com/leaflet-0.6.4/images';
+
         var geocoder = new gmaps.Geocoder();
+        var marker = null;
 
         var goToAddress = function(map, address) {
             if (ga) {
                 ga('send', 'event', 'map', 'go');
             }
 
+            if (marker) {
+                map.removeLayer(marker);
+                marker = null;
+            }
+
             geocoder.geocode({address: address}, function(results, status) {
                 if (status === gmaps.GeocoderStatus.OK) {
                     var loc = results[0].geometry.location;
-                    map.setView([loc.lat(), loc.lng()], 14);
+                    var latLng = [loc.lat(), loc.lng()];
+
+                    marker = L.marker(latLng);
+                    map.addLayer(marker);
+
+                    map.setView(latLng, 14);
                 }
             });
         };
